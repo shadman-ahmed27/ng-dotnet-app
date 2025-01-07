54e7f7db1b6f7413fc8c2230066c871147fb4269
Shadman Ahmed <Shadifmg@gmail.com>
2025-01-06 18:28:49 -0600

    feat: created navbar with the home page link, login and register buttons, that will link in the future

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, RouterOutlet, MatButtonModule, NavbarComponent],
+  imports: [
+    MatToolbarModule,
+    RouterOutlet,
+    MatButtonModule,
+    NavbarComponent,
+    MatIconModule,
+    RouterLink,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
