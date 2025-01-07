e59fe425b444df2b9b6b0f61c90349a08ac3ac72
Shadman Ahmed <Shadifmg@gmail.com>
2025-01-07 16:43:49 -0600

    feat: created the register block. added form fields of email, full name and roles with icons. styled the features. imported libraries into routes

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MatFormField } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatInputModule, MatIconModule, MatSelectModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
