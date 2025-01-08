b5ecd3e8a0cf429b4a7eff3f7ba680945da09313
Shadman Ahmed <Shadifmg@gmail.com>
2025-01-07 18:13:46 -0600

    feat: added visibility button with functionality for password and confirm pass. created the validator requirement so the form can function correctly and keep the form from messing up with wrong values are entered

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
-import { MatFormField } from '@angular/material/form-field';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink, MatInputModule, MatIconModule, MatSelectModule],
+  imports: [
+    RouterLink,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+  fb = inject(FormBuilder);
+  registerForm!: FormGroup;
+  router = inject(Router);
+  confirmPasswordHide: boolean = true;
+  passwordHide: boolean = true;
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      email: ['', Validators.required, Validators.email],
+      fullName: ['', Validators.required],
+      roles: [''],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+}
